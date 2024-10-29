af2254c9f5fd88e4b984ca9428fa5e4b6e328e58
naklida172 <homewer2016@gmail.com>
2024-10-29 12:11:28 +0600

    basic js idea structure

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', loadTasks);
+
+function loadTasks() {
+    return null;
+}
+function addTask() {
+    return null;
+}
+function deleteTask(id) {
+    return null;
+}
+function filterTasks(status) {
+    return null;
+}
+function saveTask() {
+    return null;
+}
